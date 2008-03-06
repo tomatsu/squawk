5bd61e16cf51a608bd99466bc6b914f481778eaa
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2008-03-06 22:31:38 +0000

    Initial code for bug #961 "Need range-checked access to raw memory"  - Note - DOESN"T HANDLE MISALIGNED ACCESSES YET.
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@56 5e201862-2652-49f7-a74a-6bc36218013a

---
+++ cldc/src/com/sun/squawk/realtime/OffsetOutOfBoundsException.java
@@ -0,0 +1,33 @@
+/*
+ * @(#)OffsetOutOfBoundsException.java	1.2 05/10/20
+ *
+ * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.sun.squawk.realtime;
+
+/**
+ * Thrown if the constructor of an {@link ImmortalPhysicalMemory},
+ * {@link LTPhysicalMemory}, {@link VTPhysicalMemory}, {@link RawMemoryAccess}, or
+ * {@link RawMemoryFloatAccess} is given an invalid address.
+ *
+ * @since 1.0.1 Becomes unchecked
+ */
+public class OffsetOutOfBoundsException extends RuntimeException {
+    
+    /**
+     * A constructor for <code>OffsetOutOfBoundsException</code>.
+     */
+    public OffsetOutOfBoundsException() {
+    }
+    
+    /**
+     * A descriptive constructor for <code>OffsetOutOfBoundsException</code>.
+     *
+     * @param description A description of the exception.
+     */
+    public OffsetOutOfBoundsException(String description) {
+        super(description);
+    }
+}
