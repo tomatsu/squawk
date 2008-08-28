ef2d787720e358c668dc278771bcdaa9256d4932
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2008-08-28 18:46:13 +0000

    Create a NB project for the c code in vmcore
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@176 5e201862-2652-49f7-a74a-6bc36218013a

---
+++ vmcore/nbproject/Makefile-Default.mk
@@ -0,0 +1,54 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+
+# Include project Makefile
+include vmcore-Makefile.mk
+
+# Object Directory
+OBJECTDIR=build/Default/GNU-Generic
+
+# Object Files
+OBJECTFILES=
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} 
+	cd . && 
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	cd . && 
+
+# Subprojects
+.clean-subprojects:
