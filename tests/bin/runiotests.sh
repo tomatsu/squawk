83afe1225125d7cbdc5ba6688748969c21ebd0a6
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2010-11-30 20:51:22 +0000

    Update IO tests to hit oracle.com instead of sun.com.
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@687 5e201862-2652-49f7-a74a-6bc36218013a

--- tests/bin/runiotests.sh
+++ tests/bin/runiotests.sh
@@ -2,4 +2,4 @@
 #
 
 #this works on all platforms
-./squawk com.sun.squawk.io.j2me.http.Protocol http://www.sun.com/index.html
+./squawk com.sun.squawk.io.j2me.http.Protocol http://www.oracle.com/index.html
