a6caa1229c4ab3ca94ef3c100bf5fea7fe510b5f
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2011-03-01 16:47:42 +0000

    Update test file
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@732 5e201862-2652-49f7-a74a-6bc36218013a

--- tests/bin/runsmoketest.sh
+++ tests/bin/runsmoketest.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-
+ 
 if [ $1 = SOCKET ]; then
     echo "Can't run smake test on SOCKET platform"
     exit 0
 fi
-
+ 
 if [ $1 = BARE_METAL ]; then
     echo "Can't run smoke test on BARE_METAL platform"
     exit 0
 fi
-
+ 
 ./squawk com.sun.squawk.Test
 STAT=$?
 # status got trunctated from 12345 to 57
