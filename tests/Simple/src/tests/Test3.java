8a7f6e23019bb24bc410db2c4d3b95cd99f7dff6
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2011-05-12 16:21:30 +0000

    - Fix Test3
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@753 5e201862-2652-49f7-a74a-6bc36218013a

--- tests/Simple/src/tests/Test3.java
+++ tests/Simple/src/tests/Test3.java
@@ -7,27 +7,21 @@ import com.sun.squawk.*;
  */
 public class Test3 {
 
-    static void peekIntArray(Address raw, int offset, int length) {
+    static void peekIntArray(Object raw, int offset, int length) {
         int end = offset + length;
         for (int i = offset; i != end; ++i) {
-            System.out.println("Unsafe.getInt(raw, " + i + ") = " + Unsafe.getInt(raw, i));
+            System.out.println("Unsafe.getInt(raw, " + i + ") = " + Unsafe.getInt(Address.fromObject(raw), i));
         }
     }
 
     public static void main(String[] args) {
         int[] arr = new int[2];
-        Address raw = Address.fromObject(arr);
 
-        peekIntArray(raw, 0, 2);
+        peekIntArray(arr, 0, 2);
 
         arr[0] = 555;
-        Unsafe.setInt(raw, 1, 666);
-        peekIntArray(raw, 0, 2);
-
-        // Unpredictable values after GC as 'arr' may have been moved but 'raw'
-        // will not have been updated (GC doesn't update Address variables).
-        System.gc();
-        peekIntArray(raw, 0, 2);
+        Unsafe.setInt(Address.fromObject(arr), 1, 666);
+        peekIntArray(arr, 0, 2);
 
         try {
             Class.forName("tests.Test3_error");
