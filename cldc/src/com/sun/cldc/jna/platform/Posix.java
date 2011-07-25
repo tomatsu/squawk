da2bc06b21340270e9b66947bc81647001129657
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2011-07-25 18:02:37 +0000

    - Attempted fix for linux socket error: Can't open library socket. OS Error: libsocket.so: cannot open shared object file: No such file or directory
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@799 5e201862-2652-49f7-a74a-6bc36218013a

--- cldc/src/com/sun/cldc/jna/platform/Posix.java
+++ cldc/src/com/sun/cldc/jna/platform/Posix.java
@@ -85,6 +85,9 @@ public class Posix extends Platform {
             commonMappings.put("resolv", "");
             commonMappings.put("net", "");
             commonMappings.put("nsl", "");
+        } else is (isLinux()) {
+            commonMappings.put("socket", "");
+            commonMappings.put("net", "");
         }
     }
 }
