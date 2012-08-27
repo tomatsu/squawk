cb157bff8fda114dd8a135d39468945a9830be00
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2012-08-27 19:27:52 +0000

    - Update test web page...
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@948 5e201862-2652-49f7-a74a-6bc36218013a

--- tests/bin/runiotests.sh
+++ tests/bin/runiotests.sh
@@ -2,4 +2,4 @@
 #
 
 #this works on all platforms
-./squawk com.sun.squawk.io.j2me.http.Protocol http://labs.oracle.com/index.html
+./squawk com.sun.squawk.io.j2me.http.Protocol http://labs.oracle.com/about.php
