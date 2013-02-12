514c287a312496754dd7d5d0fe75abbe334acd54
derek_white <derek_white@5e201862-2652-49f7-a74a-6bc36218013a>
2013-02-12 20:30:01 +0000

    Find a new html page to download...
    git-svn-id: file:///home/tomatsu/sandbox/tmp/squawk-mirror/trunk@969 5e201862-2652-49f7-a74a-6bc36218013a

--- tests/bin/runiotests.sh
+++ tests/bin/runiotests.sh
@@ -2,4 +2,4 @@
 #
 
 #this works on all platforms
-./squawk com.sun.squawk.io.j2me.http.Protocol http://labs.oracle.com/about.php
+./squawk com.sun.squawk.io.j2me.http.Protocol http://squawk-sqa2.us.oracle.com:8080/
